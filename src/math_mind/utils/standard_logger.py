572050294d33b8b5c70a4dd6fada1cb5f4d24ebd
asafbigel <asafbigel@gmail.com>
2026-03-08 19:58:24 +0200

    Update import path for Logger and LoggerProvider

--- src/math_mind/utils/standard_logger.py
+++ src/math_mind/utils/standard_logger.py
@@ -3,7 +3,7 @@ import logging
 import sys
 from datetime import datetime, timezone
 
-from interfaces.logger import Logger, LoggerProvider
+from math_mind.interfaces.logger import Logger, LoggerProvider
 
 
 class JsonFormatter(logging.Formatter):
